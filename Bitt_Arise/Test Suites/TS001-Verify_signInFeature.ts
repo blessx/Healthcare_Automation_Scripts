8470e80266c3b20e4613fda62e41b8b74b8ca447
blessx <blezel_tajor@ligph.com>
2020-09-02 12:37:43 +0800

    healthcare_app_katalon_script_for_login

---
+++ Bitt_Arise/Test Suites/TS001-Verify_signInFeature.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS001-Verify_signInFeature</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>084eea9e-3a93-49d2-bd4c-8d5b55c52d35</testSuiteGuid>
+   <testCaseLink>
+      <guid>d88767d9-ab9f-407f-88d9-881d9f762b7e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01-Launching_Application</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>15f0a115-ef34-4e28-83b8-e1014c6d0cd4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Verify_Login_Screen/TCLS03-validSignin_Static</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
